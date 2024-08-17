c4a84f0b2ee3c8de478e0c56ea7c92c8ca3ead75
David Owen <david.owen@csuglobal.edu>
2024-08-17 07:25:01 -0500

    AUTO_DB check/create created

--- src/PackageCuz/DatabaseConnection.java
+++ src/PackageCuz/DatabaseConnection.java
@@ -1,19 +1,37 @@
 package PackageCuz;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DatabaseConnection {
-    
-    public static void getAutoConnection() throws SQLException {
 
-        String url = "jdbc:mysql://localhost:3306/AutoInventory";
-        String user = "root";
-        String password = "Password1!";
+    private static final String AUTO_URL = "jbdc:mysql://localhost:3306/";
+    private static final String USER = "root";
+    private static final String PASSWORD = "Password1!";
 
-        
-
-    }
-
+    public static String getDbUrl() {
+        return AUTO_URL;
     }
     
+    public static Connection createDatabase(String AutoDatabase) {
+        Connection connection = null;
+
+        try {
+            connection = DriverManager.getConnection(getDbUrl(), USER, PASSWORD);
+            Statement stmt = connection.createStatement();
+
+            String createDBQuery = "CREATE DATABASE IF NOT EXISTS " + AutoDatabase;
+            stmt.executeUpdate(createDBQuery);
+            System.out.println("Database '" + AutoDatabase + "' checked/created successfully.");
+
+            connection = DriverManager.getConnection(getDbUrl(), USER, PASSWORD);
+            System.out.println("Connected to the database '" + AutoDatabase + "' successfully.");
+            
+        } catch (SQLException e) {
+        }
+
+        return connection;            
+    }    
 }
