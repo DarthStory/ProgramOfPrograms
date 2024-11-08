8c86a7b5ec14dab49c4eec757929aa1e398ae8e5
David Owen <david.owen@csuglobal.edu>
2024-11-08 12:24:21 -0600

    update

--- src/AutomobileInventory/DBInteractions/TestMain.java
+++ src/AutomobileInventory/DBInteractions/TestMain.java
@@ -12,7 +12,7 @@ public class TestMain {
         manager.loadFromFile();
 
         // Validate users
-        System.out.println(manager.validateUser("user1", "password1"));  // Should return true
+        System.out.println(manager.validateUser("user12", "password1"));  // Should return true
         //System.out.println(manager.validateUser("user2", "wrongpassword"));  // Should return false
     }
 }
